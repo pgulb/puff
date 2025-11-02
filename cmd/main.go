ecdcdd0dbd70c91d1e9baac01ea46ea31296f709
Piotr Gulbinowicz <pgulbinowicz@imgw.pl>
2025-11-02 19:33:33 +0100

    move handling --add into separate file

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	puff "github.com/pgulb/puff"
 )
@@ -81,128 +80,7 @@ func main() {
 
 	// puff --add
 	if *installRepo != "" {
-		fmt.Printf("installing %s\n", *installRepo)
-		found := false
-		for _, repo := range *puff.AvailableRepos() {
-			if repo.Path == *installRepo {
-				release, err := puff.GetLatestRelease(&repo, ghPat)
-				if err != nil {
-					fmt.Println(err.Error())
-					log.Fatal(err.Error())
-				}
-				fmt.Printf("version found: %s\n", release.Version)
-				metadata, err := puff.GetMetadata(cfgDir)
-				if err != nil {
-					fmt.Println(err.Error())
-					log.Fatal(err.Error())
-				}
-				added, err := puff.AddMetaIfNotExists(metadata, &repo, release, nil)
-				if err != nil {
-					fmt.Println(err.Error())
-					log.Fatal(err.Error())
-				}
-				if added {
-					err = puff.SaveMetadata(metadata, cfgDir)
-					if err != nil {
-						fmt.Println(err.Error())
-						log.Fatal(err.Error())
-					}
-					err := puff.DownloadBinary(cfgDir, &repo, release, ghPat)
-					if err != nil {
-						fmt.Println(err.Error())
-						log.Fatal(err.Error())
-					}
-					fmt.Printf(
-						"%s at version %s successfully installed!\n",
-						repo.Path,
-						release.Version,
-					)
-				} else {
-					log.Printf("%s at version %s already installed\n", repo.Path, release.Version)
-					fmt.Printf("%s at version %s already installed\n", repo.Path, release.Version)
-				}
-				found = true
-				break
-			}
-		}
-		if !found {
-			fmt.Println("binary not found in featured repos")
-			log.Println("binary not in featured repos")
-			ghResp, err := puff.GetLatestReleaseAssets(*installRepo, ghPat)
-			if err != nil {
-				fmt.Println(err.Error())
-				log.Fatal(err.Error())
-			}
-			if ghResp != nil {
-				if ghResp.Assets != nil {
-					if len(ghResp.Assets) == 0 {
-						log.Println("no assets found in release")
-						fmt.Println("no assets found in release")
-						return
-					}
-					fmt.Println("\nAvailable binaries:")
-					for _, v := range ghResp.Assets {
-						fmt.Println(v.Name)
-					}
-					nameParts := puff.PromptForNameParts()
-					for _, v := range ghResp.Assets {
-						containsAll := false
-						for _, part := range nameParts {
-							if strings.Contains(v.Name, part) {
-								containsAll = true
-							} else {
-								containsAll = false
-								break
-							}
-						}
-						if containsAll {
-							metadata, err := puff.GetMetadata(cfgDir)
-							if err != nil {
-								fmt.Println(err.Error())
-								log.Fatal(err.Error())
-							}
-							repo := &puff.Repo{Path: *installRepo}
-							release := &puff.Release{
-								Version: ghResp.Version,
-								Link:    v.URL,
-							}
-							added, err := puff.AddMetaIfNotExists(metadata,
-								repo,
-								release,
-								nameParts,
-							)
-							if added {
-								err = puff.SaveMetadata(metadata, cfgDir)
-								if err != nil {
-									fmt.Println(err.Error())
-									log.Fatal(err.Error())
-								}
-								err := puff.DownloadBinary(cfgDir, repo, release, ghPat)
-								if err != nil {
-									fmt.Println(err.Error())
-									log.Fatal(err.Error())
-								}
-								fmt.Printf(
-									"%s at version %s successfully installed!\n",
-									repo.Path,
-									release.Version,
-								)
-							} else {
-								log.Printf("%s at version %s already installed\n", repo.Path, release.Version)
-								fmt.Printf("%s at version %s already installed\n", repo.Path, release.Version)
-							}
-							break
-						}
-					}
-				} else {
-					log.Println("no assets found in release")
-					fmt.Println("no assets found in release")
-				}
-			} else {
-				log.Println("no release found")
-				fmt.Println("no release found")
-			}
-		}
+		puff.Add(cfgDir, installRepo, ghPat)
 		return
 	}
 }
