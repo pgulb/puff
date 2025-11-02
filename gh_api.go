b4d2882578ff74355f01fa2fe33be7ef249c5973
Piotr Gulbinowicz <pgulbinowicz@imgw.pl>
2025-11-02 17:02:23 +0100

    move creation of http client to another func and add binary downloading

--- gh_api.go
+++ gh_api.go
@@ -3,9 +3,13 @@ package puff
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
 	"regexp"
 	"time"
 )
@@ -25,17 +29,25 @@ type GithubResponse struct {
 	}
 }
 
+// returns authenticated *http.Client and *http.Request
+func AuthedClient(url string, ghPat string) (*http.Client, *http.Request, error) {
+	c := http.DefaultClient
+	c.Timeout = 15 * time.Second
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+ghPat)
+	return c, req, nil
+}
+
 // finds latest version and download link for a Repo
 func GetLatestRelease(repo *Repo, ghPat string) (*Release, error) {
 	RepoUrl, err := url.JoinPath("https://api.github.com", "repos", repo.Path, "releases/latest")
-	c := http.DefaultClient
-	c.Timeout = 15 * time.Second
-
-	req, err := http.NewRequest("GET", RepoUrl, nil)
+	c, req, err := AuthedClient(RepoUrl, ghPat)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Authorization", "Bearer "+ghPat)
 
 	resp, err := c.Do(req)
 	if err != nil {
@@ -68,4 +80,39 @@ func GetLatestRelease(repo *Repo, ghPat string) (*Release, error) {
 	} else {
 		return nil, err
 	}
+}
+
+// downloads a binary and puts into bin directory
+func DownloadBinary(cfgDir string, repo *Repo, release *Release, ghPat string) error {
+	c, req, err := AuthedClient(release.Link, ghPat)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		binName, err := BinNameFromPath(repo)
+		if err != nil {
+			return err
+		}
+		savePath := filepath.Join(cfgDir, "bin", binName)
+		log.Printf("writing %s to %s\n", binName, savePath)
+		err = os.WriteFile(savePath, bodyBytes, 0750)
+		if err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("API returned status %v", resp.StatusCode)
+	}
+
+	return nil
 }
