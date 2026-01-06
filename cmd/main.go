450e8601b4dede51b0858703ffe813bb3fc0fff7
Piotr Gulbinowicz <pgulbinowicz@imgw.pl>
2026-01-06 14:23:55 +0100

    add show command to show locally installed bins

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,7 @@ import (
 func printHelp() {
 	fmt.Println("puff - a tool for managing binary installations")
 	fmt.Println("Usage:")
+	fmt.Println("  puff show -> show installed binaries")
 	fmt.Println("  puff list -> list repositories with pre-added regex for release name")
 	fmt.Println("  puff add <repo> -> install binary from repo")
 	fmt.Println("  puff upd -> update all installed binaries")
@@ -72,6 +73,20 @@ func main() {
 		printHelp()
 	}
 	switch os.Args[1] {
+	case "show":
+		metadata, err := puff.GetMetadata(cfgDir)
+		if err != nil {
+			fmt.Println(err.Error())
+			log.Fatal(err.Error())
+		}
+		if len(metadata.Metadata) == 0 {
+			fmt.Println("No installed binaries found.")
+			return
+		} else {
+			for _, v := range metadata.Metadata {
+				fmt.Printf("- %s (version: %s)\n", v.Path, v.Version)
+			}
+		}
 	case "list":
 		fmt.Println("Available repositories: ")
 		for _, repo := range *puff.AvailableRepos() {
