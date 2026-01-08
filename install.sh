6c1db19facd77fe5db1e32c97efe1a8074ac91d3
Piotr Gulbinowicz <pgulbinowicz@imgw.pl>
2026-01-08 11:25:02 +0100

    improve PATH adding in installation

--- install.sh
+++ install.sh
@@ -7,6 +7,30 @@ chmod -R 750 ~/.config/puff
 curl -sSL https://github.com/pgulb/puff/releases/latest/download/puff -o ~/.config/puff/bin/puff
 chmod +x ~/.config/puff/bin/puff
 
-echo "export PATH=\$PATH:${HOME}/.config/puff/bin" >> ~/.bashrc
-echo "export PATH=\$PATH:${HOME}/.config/puff/bin" >> ~/.zshrc
+PUFF_PATH="export PATH=\$PATH:${HOME}/.config/puff/bin"
+
+# Check and update .bashrc
+if [[ -f ~/.bashrc ]]; then
+    if ! grep -q "${HOME}/.config/puff/bin" ~/.bashrc; then
+        echo "$PUFF_PATH" >> ~/.bashrc
+        echo ".bashrc updated."
+    else
+        echo "PATH already set in .bashrc."
+    fi
+else
+    echo ".bashrc not found, skipping."
+fi
+
+# Check and update .zshrc
+if [[ -f ~/.zshrc ]]; then
+    if ! grep -q "${HOME}/.config/puff/bin" ~/.zshrc; then
+        echo "$PUFF_PATH" >> ~/.zshrc
+        echo ".zshrc updated."
+    else
+        echo "PATH already set in .zshrc."
+    fi
+else
+    echo ".zshrc not found, skipping."
+fi
+
 echo "Puff CLI installed. Restart your terminal or run 'source ~/.bashrc' or 'source ~/.zshrc'."
