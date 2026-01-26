161a3126c6dd7439184d8e0fc00bc6b821d9a0f9
Piotr Gulbinowicz <pgulbinowicz@imgw.pl>
2026-01-26 10:31:23 +0100

    move code for each cli command to a function

--- cli.go
+++ cli.go
@@ -68,74 +68,98 @@ func setup() (string, string) {
 	return cfgDir, ghPat
 }
 
+// list installed binaries
+func listCmd(cfgDir string) {
+	metadata, err := GetMetadata(cfgDir)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	if len(metadata.Metadata) == 0 {
+		log.Fatal("No installed binaries found.")
+	} else {
+		for _, v := range metadata.Metadata {
+			fmt.Printf("- %s (version: %s)\n", v.Path, v.Version)
+		}
+	}
+}
+
+// search featured repositories
+func searchCmd() {
+	if len(os.Args) < 3 {
+		for _, repo := range *AvailableRepos() {
+			fmt.Printf("- %s - %s\n", repo.Path, repo.Desc)
+		}
+	} else {
+		searchTerm := os.Args[2]
+		for _, repo := range *AvailableRepos() {
+			if strings.Contains(
+				strings.ToLower(repo.Path),
+				strings.ToLower(searchTerm)) || strings.Contains(
+				strings.ToLower(repo.Desc), strings.ToLower(searchTerm)) {
+				fmt.Printf("- %s - %s\n", repo.Path, repo.Desc)
+			}
+		}
+	}
+}
+
+// install binary/binaries
+func addCmd(cfgDir string, ghPat string) {
+	if len(os.Args) < 3 {
+		printHelp()
+	}
+	reposToAdd := os.Args[2:]
+	for _, installRepo := range reposToAdd {
+		err := Add(cfgDir, &installRepo, ghPat)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+}
+
+// update all installed binaries and puff itself
+func updCmd(cfgDir string, ghPat string) {
+	fmt.Println("Updating all installed binaries")
+	metadata, err := GetMetadata(cfgDir)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	err = Update(cfgDir, ghPat, metadata)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+// remove installed binary/binaries
+func rmCmd(cfgDir string) {
+	if len(os.Args) < 3 {
+		printHelp()
+	}
+	reposToRemove := os.Args[2:]
+	for _, removeRepo := range reposToRemove {
+		err := Remove(cfgDir, &removeRepo)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+}
+
 func Run() {
 	cfgDir, ghPat := setup()
 
-	// commands
 	if len(os.Args) < 2 {
 		printHelp()
 	}
 	switch os.Args[1] {
 	case "list":
-		metadata, err := GetMetadata(cfgDir)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		if len(metadata.Metadata) == 0 {
-			log.Fatal("No installed binaries found.")
-		} else {
-			for _, v := range metadata.Metadata {
-				fmt.Printf("- %s (version: %s)\n", v.Path, v.Version)
-			}
-		}
+		listCmd(cfgDir)
 	case "search":
-		if len(os.Args) < 3 {
-			for _, repo := range *AvailableRepos() {
-				fmt.Printf("- %s - %s\n", repo.Path, repo.Desc)
-			}
-		} else {
-			searchTerm := os.Args[2]
-			for _, repo := range *AvailableRepos() {
-				if strings.Contains(
-					strings.ToLower(repo.Path),
-					strings.ToLower(searchTerm)) || strings.Contains(
-					strings.ToLower(repo.Desc), strings.ToLower(searchTerm)) {
-					fmt.Printf("- %s - %s\n", repo.Path, repo.Desc)
-				}
-			}
-		}
+		searchCmd()
 	case "add":
-		if len(os.Args) < 3 {
-			printHelp()
-		}
-		reposToAdd := os.Args[2:]
-		for _, installRepo := range reposToAdd {
-			err := Add(cfgDir, &installRepo, ghPat)
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-		}
+		addCmd(cfgDir, ghPat)
 	case "upd":
-		fmt.Println("Updating all installed binaries")
-		metadata, err := GetMetadata(cfgDir)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		err = Update(cfgDir, ghPat, metadata)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+		updCmd(cfgDir, ghPat)
 	case "rm":
-		if len(os.Args) < 3 {
-			printHelp()
-		}
-		reposToRemove := os.Args[2:]
-		for _, removeRepo := range reposToRemove {
-			err := Remove(cfgDir, &removeRepo)
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-		}
+		rmCmd(cfgDir)
 	case "version", "--version", "-v":
 		fmt.Println(Version)
 	default:
